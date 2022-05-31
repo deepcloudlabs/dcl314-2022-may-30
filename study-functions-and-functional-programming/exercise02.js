4faddaad45c22fbe3c01985cef0781482113384d
deepcloudlabs <deepcloudlabs@gmail.com>
2022-05-31 10:50:13 +0300

    Update exercise02.js

--- study-functions-and-functional-programming/exercise02.js
+++ study-functions-and-functional-programming/exercise02.js
@@ -4,6 +4,27 @@
 // 3. Generator Function!
 // Abstraction Level
 // Built-in HoF: filter/map/reduce
+function* filter(array, predicate_fun) {
+    for (let value of array) {
+        if (predicate_fun(value))
+            yield value;
+    }
+}
+
+function* map(array, map_fun) {
+    for (let value of array) {
+        yield map_fun(value);
+    }
+}
+
+function reduce(array, reduce_fun, initial_value) {
+    let result = initial_value;
+    for (let value of array) {
+        result = reduce_fun(result, value);
+    }
+    return result;
+}
+
 let circles = [
     {x: 1, y: 1, radius: 100, color: 'red'},
     {x: -1, y: 0, radius: 200, color: 'blue'},
@@ -20,11 +41,21 @@ for (let circle of circles) {
     }
 }
 console.log(`Sum of the circle areas in first quadrant: ${sum}`)
-sum =
-    circles.filter(circle => circle.x >= 0)
-        .filter(circle => circle.y >= 0)
-        .filter(circle => circle.color === "red")
-        .map(circle => circle.radius)
-        .map(radius => Math.PI * radius * radius)
-        .reduce((x, y) => x + y, 0)
+let first_quadrant = circle => {
+    console.log("first_quadrant")
+    return circle.x >= 0 && circle.y >= 0;
+}
+let redish_circle = circle => {
+    console.log("redish_circle")
+    return circle.color === "red";
+}
+let to_circle_area = circle => {
+    console.log("to_circle_area")
+    return Math.PI * circle.radius * circle.radius;
+}
+let to_sum = (x, y) => {
+    console.log("to_sum")
+    return x + y;
+}
+sum = reduce(map(filter(filter(circles, first_quadrant), redish_circle), to_circle_area), to_sum, 0);
 console.log(`Sum of the circle areas in first quadrant: ${sum}`)
