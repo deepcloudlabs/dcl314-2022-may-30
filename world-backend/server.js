f8eb56313c3dd7f2e6897a2419349f7862a06522
deepcloudlabs <deepcloudlabs@gmail.com>
2022-06-01 12:08:50 +0300

    Update server.js

--- world-backend/server.js
+++ world-backend/server.js
@@ -1,6 +1,24 @@
 const port = 9100;
 const mongoose = require("mongoose");
 
+//region express initialization
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
+
+const api = express();
+api.use(bodyParser.json({
+    limit: "5mb"
+}))
+api.use(logger('dev'));
+api.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "HEAD,OPTIONS,POST,PUT,PATCH,DELETE,GET");
+    res.header("Access-Control-Allow-Headers", "Origin,Content-Type,Accept");
+    next();
+})
+//endregion
+
 //region integration
 mongoose.connect("mongodb://localhost:27017/world",{
     "useNewUrlParser": true,
